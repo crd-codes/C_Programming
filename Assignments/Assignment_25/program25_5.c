1950fc49311efd6e209da66c176fb7226c513fc7
crd-codes <dugadechakradhar@gmail.com>
2025-11-18 23:59:27 +0530

    Assignment_25 programs added

---
+++ Assignments/Assignment_25/program25_5.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+typedef int * IPTR ;
+
+void Display(int Arr[], int iSize)
+{
+    int iCnt = 0;
+
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        if(Arr[iCnt] % 11 == 0)
+        {
+            printf("%d\n", Arr[iCnt]);
+        }        
+    }
+        
+}
+int main()
+{
+    int iLength = 0, iCnt = 0;
+    IPTR iPtr = NULL;
+
+    printf("Enter the number of elements :");
+    scanf("%d", &iLength);
+
+    // Step 1 : Allocate the memory
+    iPtr = (IPTR)malloc(iLength * sizeof(int));
+    if(NULL == iPtr)
+    {
+        printf("Unable to allocate the memory \n");
+        return -1;
+    }
+
+    printf("Enter %d the values : \n", iLength);
+
+    for(iCnt = 0; iCnt < iLength; iCnt++)
+    {
+        scanf("%d",&iPtr[iCnt]);
+    }
+
+    Display(iPtr, iLength);
+
+    free(iPtr);
+
+    return 0;
+}
