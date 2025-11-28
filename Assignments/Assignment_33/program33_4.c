2a21d79459dc3d7dc6ee31c788c8d77a2067ca2a
crd-codes <dugadechakradhar@gmail.com>
2025-11-28 10:33:55 +0530

    Assignment_33 programs added

---
+++ Assignments/Assignment_33/program33_4.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+#define TRUE 1
+#define FALSE 0
+
+typedef int BOOL;
+
+BOOL ChkSmall(char ch)
+{
+    if(ch >= 'a' && ch <= 'z')
+    {
+        return TRUE;
+    }
+    else if(ch >= 'A' && ch <= 'Z')
+    {
+        return FALSE;
+    }
+
+}
+
+int main()
+{
+    char cValue = '\0';
+    BOOL bRet = FALSE;
+
+    printf("Enter the character : \n");
+    scanf(" %c", &cValue);
+
+    bRet = ChkSmall(cValue);
+
+    if(bRet == TRUE)
+    {
+        printf("It is small case Character");
+    }
+    else
+    {
+        printf("It is not a small case Character");
+
+    }    
+
+    return 0;
+}
---
+++ Assignments/Assignment_33/program33_5.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+
+#define TRUE 1
+#define FALSE 0
+
+typedef int BOOL;
+
+void DisplaySchedule(char chDiv)
+{
+    if(chDiv == 'a' || chDiv == 'A')
+    {
+        printf("Your exam is at 7 AM\n");
+    }
+    else if(chDiv == 'b' || chDiv == 'B')
+    {
+        printf("Your exam is at 8.30 AM\n");   
+    }
+    else if(chDiv == 'c' || chDiv == 'C')
+    {
+        printf("Your exam is at 9.20 AM\n");
+    }
+    else if(chDiv == 'd' || chDiv == 'D')
+    {
+        printf("Your exam is at 10.30 AM\n");
+    }
+    else
+    {
+        printf("Invalid division\n");       
+    }
+    
+}
+
+int main()
+{
+    char cValue = '\0';
+
+    printf("Enter your division : \n");
+    scanf(" %c", &cValue);
+
+    DisplaySchedule(cValue); 
+
+    return 0;
+}
